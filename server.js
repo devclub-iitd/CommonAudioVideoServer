65e2542363ab142e61c1833a0677c9f1298dc0af
Aman Verma <amanverma1729@gmail.com>
2020-05-12 00:34:21 +0530

    mongoDB working..

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const db_uri = process.env.DB_URI
+const db_uri = process.env.DB_URI;
 
-mongoose.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err))
+mongoose
+  .connect(db_uri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
