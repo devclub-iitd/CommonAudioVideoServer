cf94d25c72ce7148c79ef9ee34979eb86dd0e0ef
Saksham <sakshamdev5@gmail.com>
2020-05-12 16:48:05 +0530

    utils added along with some badges

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
 
+import {MONGODB_URI} from './utils/secrets';
+
+// Create Express server.
 const app = express();
 
+// Connect to MongoDB
+const mongoUrl: string = MONGODB_URI;
+mongoose
+  .connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((): void => {
+    throw new Error('Cannot Connect To MongoDB');
+  });
+
+// Express configuration
 app.set('port', process.env.PORT || 5000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Common Audio Video Streaming Server !!');
---
+++ src/utils/secrets.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+
+if (fs.existsSync('src/.env')) {
+  dotenv.config({path: 'src/.env'});
+}
+
+export const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  'mongodb://saksham:saksham5@ds343887.mlab.com:43887/iitd-connect';
