dc41892e2b13617fcfb939fac4b0c5f997c93c41
Saksham <sakshamdev5@gmail.com>
2020-05-15 17:47:44 +0530

    restructured

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,16 @@
 import * as express from 'express';
-import * as mongoose from 'mongoose';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
-import * as crypto from 'crypto';
-import * as multer from 'multer';
-import * as GridFsStorage from 'multer-gridfs-storage';
-import * as Grid from 'gridfs-stream';
-import * as path from 'path';
-
-import {MONGODB_URI} from './utils/secrets';
+import {Request, Response, NextFunction} from 'express';
+import routes from './routes';
+// import * as crypto from 'crypto';
+// import * as multer from 'multer';
+// import * as GridFsStorage from 'multer-gridfs-storage';
+// import * as Grid from 'gridfs-stream';
+// import * as path from 'path';
 
 // Create Express server.
 const app = express();
-
-// Connect to MongoDB
-const mongoUrl: string = MONGODB_URI;
-mongoose
-  .connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((): void => {
-    throw new Error('Cannot Connect To MongoDB');
-  });
 
 // Express configuration
 app.set('port', process.env.PORT || 5000);
@@ -29,77 +19,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Common Audio Video Streaming Server !!');
+app.get('/', (_, res) => {
+  res.json({
+    data: null,
+    message: 'Hooray! Welcome to Common Audio Server!',
+  });
 });
 
-const conn = mongoose.connection;
+//* Takes Care of All The Routing
+app.use('/api', routes);
 
-let gfs: any;
-
-conn.once('open', () => {
-  // Init stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+app.use('/healthz', (_0: Request, res: Response) => {
+  res.send('Ok, Healthy!');
 });
 
-// Create storage engine
-const storage = new GridFsStorage({
-  url: mongoUrl,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads',
-        };
-        resolve(fileInfo);
-      });
+app.use((_req: Request, _res: Response, next: NextFunction): void => {
+  const err = new Error('Page Not Found');
+  next(err);
+});
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+    res.status(500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
     });
-  },
-});
-const upload = multer({storage});
-
-// @route POST /upload
-// @desc  Uploads file to DB
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({file: req.file});
-  //res.redirect('/');
-});
-
-// @route GET /files
-// @desc  Display all files in JSON
-app.get('/files', (req, res) => {
-  gfs.files.find().toArray((err: any, files: any) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist',
-      });
-    }
-
-    // Files exist
-    return res.json(files);
-  });
-});
-
-// @route GET /files/:filename
-// @desc  Display single file object
-app.get('/files/:filename', (req, res) => {
-  gfs.files.findOne({filename: req.params.filename}, (err: any, file: any) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists',
-      });
-    }
-    // File exists
-    return res.json(file);
-  });
-});
+  }
+);
 
 export default app;
